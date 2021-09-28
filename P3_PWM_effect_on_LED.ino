54cfa72dfb68208ecbd0eafe42307752ac83f71a
Sid <64964929+sid-py@users.noreply.github.com>
2021-09-28 22:56:21 +0200

    Update P3_PWM_effect_on_LED.ino

--- P3_PWM_effect_on_LED.ino
+++ P3_PWM_effect_on_LED.ino
@@ -8,14 +8,14 @@ void setup()
 
 void loop()
 {
-  for (int a = 0; a < 256 ; a++)
+  for (int a = 0; a < 256 ; a++) // Gradual increase in duty cycle
   {
     analogWrite(3,a);
     delay(d);
     
   }
   
-  for (int a = 255; a >= 0; a--)
+  for (int a = 255; a >= 0; a--) // Gradual decrease in duty cycle 
      {
     analogWrite(3,a);
     delay(d);
